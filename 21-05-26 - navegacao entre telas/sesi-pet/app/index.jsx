b95d2427a724aa02293cf74dc28c416b9548f1dd
ALUNO3A <luana.r.pereira7@aluno.senai.br>
2026-05-21 11:03:56 -0300

    2105

---
+++ 21-05-26 - navegacao entre telas/sesi-pet/app/index.jsx
@@ -0,0 +1,16 @@
+
+import { View } from "react-native";
+
+import Logo from "../assets/images/logo.pen1.jpeg";
+
+export default function Index() {
+  
+  return (
+    
+      <View>
+
+      </View>
+
+      
+  
+)};
